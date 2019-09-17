716f42c6a988c1274cb1e27e723dc81403b9b494
Andrii <Dolanda94@gmail.com>
2019-09-17 21:30:23 +0300

    create game.h

---
+++ game.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Game {
+public:
+    enum Direction { UP, DOWN, LEFT, RIGHT };
+    void draw(Painter &);
+    void tick();
+    void restart();
+    void keyEvent(Direction);
+};
